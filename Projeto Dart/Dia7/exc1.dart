fb81564daab78e07cec31e160a2e7d62b5b44f9d
PC1UFPA <oliveirajoaopdr@gmail.com>
2023-08-02 12:06:33 -0300

    upando exc1 day7

--- Projeto Dart/Dia7/exc1.dart
+++ Projeto Dart/Dia7/exc1.dart
@@ -1,60 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CenteredContainer(),
-    );
-  }
-}
-
-class CenteredContainer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  runApp(MaterialApp(
+    home: Scaffold(
       body: Center(
         child: Container(
-          width: MediaQuery.of(context).size.width / 2,
-          height: 200,
-          margin: EdgeInsets.all(20),
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.purple, Colors.pink],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.purple.withOpacity(0.5),
-                blurRadius: 10,
-                spreadRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ],
-            image: DecorationImage(
-              image: AssetImage('assets/background_image.jpg'),
-              fit: BoxFit.cover,
+          child: Text(
+            'Flutterzadas',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
             ),
           ),
-          child: Center(
-            child: Text(
-              'Essa cor é bonita.',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
+          color: Colors.blue,
+          alignment: Alignment.center,
+          width: 500,
+          height: 300,
+          transform: Matrix4.rotationZ(0.05),
         ),
       ),
-    );
-  }
+    ),
+  ));
 }
