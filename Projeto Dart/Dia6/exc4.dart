b85216ca85b12c4c29346b92c81c8c99741ba94f
PC1UFPA <oliveirajoaopdr@gmail.com>
2023-08-02 09:28:02 -0300

    upando exc4 day6

--- Projeto Dart/Dia6/exc4.dart
+++ Projeto Dart/Dia6/exc4.dart
@@ -18,155 +18,162 @@ void main() {
       'Canapés de Salmão com Cream Cheese',
     ],
   };
-  
+
   final int? categoria = null;
 
-  runApp(MaterialApp(
-    if (categoria == null)
+  runApp(
+    MaterialApp(
       // Todas as categorias
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Minhas receitas'),
-        ),
-        body: ListView(
-          children: [
-            Center(
-            child: Text(
-              'Sobremesas',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Sobremesas']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          Center(
-            child: Text(
-              'Pratos principais',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Pratos principais']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          Center(
-            child: Text(
-              'Aperitivos',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          SizedBox(height: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Aperitivos']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          ],
-        ),
-      );
-    } else if (categoria == 1) {
-      // Categoria Sobremesas
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Sobremesas'),
-        ),
-        body: ListView(
-          children: [
-            Center(
-            child: Text(
-              'Sobremesas',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Sobremesas']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          ],
-        ),
-      );
-    } else if (categoria == 2) {
-      // Categoria Pratos principais
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Pratos principais'),
-        ),
-        body: ListView(
-          children: [
-          Center(
-            child: Text(
-              'Pratos principais',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Pratos principais']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          ],
-        ),
-      );
-    } else if (categoria == 3) {
-      // Categoria Aperitivos
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Aperitivos'),
-        ),
-        body: ListView(
-          children: [
-          Center(
-            child: Text(
-              'Aperitivos',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: dados['Aperitivos']!.map((receita) {
-              return Text(
-                receita,
-                style: TextStyle(fontSize: 18),
-              );
-            }).toList(),
-          ),
-          ],
-        ),
-      );
-    } else {
-      // Categoria inválida
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Minhas receitas'),
-        ),
-        body: Center(
-          child: Text('A categoria selecionada é inválida.'),
-        ),
-      );
-    }
-  ));
+      home: categoria == null
+          ? Scaffold(
+              appBar: AppBar(
+                title: Text('Minhas receitas'),
+              ),
+              body: ListView(
+                children: [
+                  Center(
+                    child: Text(
+                      'Sobremesas',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: dados['Sobremesas']!.map((receita) {
+                      return Text(
+                        receita,
+                        style: TextStyle(fontSize: 18),
+                      );
+                    }).toList(),
+                  ),
+                  Center(
+                    child: Text(
+                      'Pratos principais',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: dados['Pratos principais']!.map((receita) {
+                      return Text(
+                        receita,
+                        style: TextStyle(fontSize: 18),
+                      );
+                    }).toList(),
+                  ),
+                  Center(
+                    child: Text(
+                      'Aperitivos',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: dados['Aperitivos']!.map((receita) {
+                      return Text(
+                        receita,
+                        style: TextStyle(fontSize: 18),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              // Categoria Sobremesas
+            )
+          : categoria == 1
+              ? Scaffold(
+                  appBar: AppBar(
+                    title: Text('Sobremesas'),
+                  ),
+                  body: ListView(
+                    children: [
+                      Center(
+                        child: Text(
+                          'Sobremesas',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: dados['Sobremesas']!.map((receita) {
+                          return Text(
+                            receita,
+                            style: TextStyle(fontSize: 18),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                  // Categoria Pratos principais
+                )
+              : categoria == 2
+                  ? Scaffold(
+                      appBar: AppBar(
+                        title: Text('Pratos principais'),
+                      ),
+                      body: ListView(
+                        children: [
+                          Center(
+                            child: Text(
+                              'Pratos principais',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 24),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children:
+                                dados['Pratos principais']!.map((receita) {
+                              return Text(
+                                receita,
+                                style: TextStyle(fontSize: 18),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                      // Categoria Aperitivos
+                    )
+                  : categoria == 3
+                      ? Scaffold(
+                          appBar: AppBar(
+                            title: Text('Aperitivos'),
+                          ),
+                          body: ListView(
+                            children: [
+                              Center(
+                                child: Text(
+                                  'Aperitivos',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: dados['Aperitivos']!.map((receita) {
+                                  return Text(
+                                    receita,
+                                    style: TextStyle(fontSize: 18),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Scaffold(
+                          appBar: AppBar(
+                            title: Text('Minhas receitas'),
+                          ),
+                          body: Center(
+                            child: Text('A categoria selecionada é inválida.'),
+                          ),
+                        ),
+    ),
+  );
 }
