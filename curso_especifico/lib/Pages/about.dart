7d65c517e9077ecb6cfcfe6f8e018b58e29ea1ff
PP <joaomestre146@gmail.com>
2023-08-26 21:30:30 -0300

    .

---
+++ curso_especifico/lib/Pages/about.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'components/menu.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              _openRightMenu(context);
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text("About me"),
+      ),
+    );
+  }
+
+  void _openRightMenu(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Menu()));
+  }
+}
---
+++ curso_especifico/lib/app/app.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../Pages/about.dart';
+import '../Pages/home.dart';
+import '../Pages/projects.dart';
+import '../Pages/skills_knowledge.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final router = GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(
+          path: '/',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: Home(),
+          ),
+        ),
+        GoRoute(
+          path: '/projetos',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: Projects(),
+          ),
+        ),
+        GoRoute(
+          path: '/conhecimentos',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: SkillsKnowledge(),
+          ),
+        ),
+        GoRoute(
+          path: '/sobre',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: About(),
+          ),
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
